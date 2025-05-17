6d6f34ba4a83b7cc645c57a0dc1bf392f4f548f2
suehyun73 <suehyun73@gmail.com>
2025-05-17 18:08:02 +0900

    feat: exception handler 기능 구현

--- app/util/exception.py
+++ app/util/exception.py
@@ -8,6 +8,16 @@ class Base(Exception):
         super().__init__(self.name)
 
 
+class FieldMissingException(Base):
+    status = 400
+    name = "필수 필드 누락"
+
+
+class ValidationException(Base):
+    status = 422
+    name = '데이터 유효성 검증 실패'
+
+
 class OAuthServerException(Base):
     status = 502
     name = "OAuth 서버 연결 오류"
