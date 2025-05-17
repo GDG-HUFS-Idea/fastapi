a3db42d08fdc93cfd266b80127cd29b59ac6cc76
suehyun73 <suehyun73@gmail.com>
2025-05-17 19:10:30 +0900

    chore: http status 변경

--- app/api/router/auth.py
+++ app/api/router/auth.py
@@ -13,10 +13,10 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get(
     path="/oauth/{provider}",
-    status_code=302,
+    status_code=307,
     response_class=RedirectResponse,
     responses={
-        302: {
+        307: {
             "description": "인증 제공자(provider)의 로그인 페이지로 성공적으로 리다이렉트됨"
         },
         400: {
@@ -27,10 +27,7 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
             "description": "ValidationException: 지원하지 않는 인증 제공자(provider) "
             "값이 전달되었거나, provider 값의 형식이 올바르지 않음"
         },
-        500: {
-            "description": "InternalLogicException: OAuth 인증 URI 생성 과정에서 "
-            "오류가 발생하거나, 원인 불명의 내부 로직 오류가 발생함"
-        },
+        500: {"description": "원인 불명의 내부 로직 오류가 발생함"},
         502: {
             "description": "OAuthServerException: 외부 OAuth 인증 서버와의 "
             "통신 중 오류가 발생하거나, OAuth 서버가 비정상적인 응답을 반환함"
